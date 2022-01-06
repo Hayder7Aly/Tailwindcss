8395cd5c348f5bef2e9c5e43701cb2edfea25d73
Hayder7Aly <007harryjutt@gmail.com>
2022-01-06 12:02:29 +0500

    learn tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center : true,
+      padding : '1.3rem'
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
